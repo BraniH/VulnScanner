8d519a852f13d2cf77bb4a72d9ec29402feadc88
Branislav Hraško <branislavh@kentico.com>
2019-10-18 15:38:50 +0200

    Login future improved

--- VulnScanner_View.py
+++ VulnScanner_View.py
@@ -2,10 +2,10 @@
 
 """Calls functionality"""
 
-import VulnScanner_Controller as FnOfScan
+import VulnScanner_Controller as ScanControl
 
 
-def save_resulst(results):
+def save_outputs(results):
     write = input("[?] Do you want to save the results (y/n)? ")
     temp = ["y", "n"]
     while write not in temp:
@@ -14,21 +14,21 @@ def save_resulst(results):
         write = write.replace(" ", "")
 
     if write == "y":
-        name_of_file = input("File name: ")
-        name_of_file += ".txt"
-        with open(name_of_file, 'w') as results_to_be_saved:
+        file_name = input("File name: ")
+        file_name += ".txt"
+        with open(file_name, 'w') as results_to_be_saved:
             for result in results:
                 print(result, file=results_to_be_saved)
-            print("\n[+] Finished! {0} results found. File name: {1}.".format(str(len(results)), name_of_file))
+            print("\n[+] Finished! {0} results found. File name: {1}.".format(str(len(results)), file_name))
 
     elif write == "n":
         print("\n [+] Back to the main menu...")
 
 
-def url_for_testing():
-    target_url_function = input("[?] Add url to test: ")
-    target_url_function = target_url_function.replace(" ", "")
-    vuln_scanner_function = FnOfScan.FunctionalityOfScan(target_url_function)
+def input_url_handler():
+    input_url = input("[?] Add url to test: ")
+    input_url = input_url.replace(" ", "")
+    controller_setting = ScanControl.ScannerController(input_url)
     login_url = input("[?]  If you want to login, add login url else, leave empty: ")
     login_url = login_url.replace(" ", "")
 
@@ -38,51 +38,51 @@ def url_for_testing():
     try:
         if login_url != "":
             data_dict = {"username": username, "password": user_password, "Login": "submit"}
-            response = vuln_scanner_function.session.post(login_url, data=data_dict)
+            response = controller_setting.session.post(login_url, data=data_dict)
     except TypeError:
         print["! Bad login url"]
 
-    return target_url_function, vuln_scanner_function
+    return input_url, controller_setting
 
 
-target_url, vuln_scanner = url_for_testing()
+target_url, scan_call = input_url_handler()
 
 while True:
-    print('\n[?] Type "url" if you want to change url for scanning')
+    print('\n[?] Type "U" if you want to change url for scanning')
     print("[?] Press 1. Full Scan")
     print("[?] Press 2. XSS Scan")
     print("[?] Press 3. Crawler")
     print("[?] Press X  Turn off program")
 
-    type_of_scan = input("\nYour choice: ")
+    program_option = input("\nYour choice: ")
 
-    if type_of_scan == "1":
+    if program_option == "1":
         print("full_scan")
-    elif type_of_scan == "2":
+    elif program_option == "2":
 
         print("[*] In progress...")
 
-        crawled_urls = vuln_scanner.crawler(target_url)
+        crawled_urls = scan_call.crawler(target_url)
         xss_result_forms = []
         xss_result_url = []
         xss_results = []
         cleaned_xss_results = []
-        counter = 1
+        results_count = 1
 
-        for b in range(0, len(crawled_urls)):
-            form = vuln_scanner.extract_forms(crawled_urls[b])
+        for crawled_url in range(0, len(crawled_urls)):
+            form = scan_call.extract_forms(crawled_urls[crawled_url])
             try:
-                if len(vuln_scanner.xss_in_foms(crawled_urls[b], form[0])) > 0:
-                    xss_result_forms = vuln_scanner.xss_in_foms(crawled_urls[b], form[0])
+                if len(scan_call.xss_in_forms(crawled_urls[crawled_url], form[0])) > 0:
+                    xss_result_forms = scan_call.xss_in_forms(crawled_urls[crawled_url], form[0])
                     xss_results.append(xss_result_forms)
-                    print(f"\r[+] {counter} possible vulnerabilities found!", end="")
-                    counter += 1
+                    print(f"\r[+] {results_count} possible vulnerabilities found!", end="")
+                    results_count += 1
 
-                if len(vuln_scanner.test_xss_link(crawled_urls[b])) > 0:
-                    xss_result_url = vuln_scanner.test_xss_link(crawled_urls[b])
+                if len(scan_call.test_xss_link(crawled_urls[crawled_url])) > 0:
+                    xss_result_url = scan_call.test_xss_link(crawled_urls[crawled_url])
                     xss_results.append(xss_result_url)
-                    print(f"\r[+] {counter} possible vulnerabilities found!", end="")
-                    counter += 1
+                    print(f"\r[+] {results_count} possible vulnerabilities found!", end="")
+                    results_count += 1
 
             except IndexError:
                 pass
@@ -91,19 +91,19 @@ while True:
             cleaned_xss_results.append(xss_site_results[0])
             print(xss_site_results[0])
 
-        save_resulst(cleaned_xss_results)
+        save_outputs(cleaned_xss_results)
 
-    elif type_of_scan == "3":
+    elif program_option == "3":
         print("[*] In progress... ")
-        crawled_urls = vuln_scanner.crawler(target_url)
-        for a in range(0, len(crawled_urls)):
-            print(crawled_urls[a])
+        crawled_urls = scan_call.crawler(target_url)
+        for crawled_url in range(0, len(crawled_urls)):
+            print(crawled_urls[crawled_url])
 
-        save_resulst(crawled_urls)
+        save_outputs(crawled_urls)
 
-    elif type_of_scan == "url":
-        target_url, vuln_scanner = url_for_testing()
-    elif type_of_scan == "X" or type_of_scan == "x":
+    elif program_option == "U" or program_option == "u":
+        target_url, scan_call = input_url_handler()
+    elif program_option == "X" or program_option == "x":
         print("[+] program is closing")
         break
     else:
